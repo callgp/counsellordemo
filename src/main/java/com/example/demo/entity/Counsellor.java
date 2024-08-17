254fdb93bc56782b5f8e83c063020e8bc096a0e0
callgp <callgp@gmail.com>
2024-08-17 18:05:01 -0400

    commiting

---
+++ src/main/java/com/example/demo/entity/Counsellor.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+public class Counsellor {
+private static int counsellor_id;
+private static String name;
+private static String email;
+private static String pwd;
+private static String phno;
+private static LocalDateTime created_date;
+private static LocalDateTime updated_date;
+}
---
+++ src/main/java/com/example/demo/entity/Enquiry.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;+
+
+import java.time.LocalDateTime;
+
+public class Enquiry {
+
+	
+	public class Counsellor {
+	/*	private static int counsellor_id;
+		private static String name;
+		private static String email;
+		private static String pwd;
+		private static String phno;
+		private static LocalDateTime created_date;
+		private static LocalDateTime updated_date;
+		public static int getCounsellor_id() {*/
+		
+		private static int enq_id;
+		private static String	student_name;
+		private static String student_phno;
+		private static String course_name;
+		private static String class_mode;
+		private static enum	enq_status;
+		private static LocalDateTime created_date;
+		private static LocalDateTime updated_date;
+
+		counsellor_id (FK)
+}
